2ddde1da7afa5a7fcf844032daa0d4cb6ce40c38
Tim Blechmann <tim@klingt.org>
2013-12-07 15:47:06 +0100

    atomic: detail - use BOOST_GCC macro

--- include/boost/lockfree/detail/atomic.hpp
+++ include/boost/lockfree/detail/atomic.hpp
@@ -18,22 +18,9 @@
 #endif
 
 // GCC supports atomic<> from version 4.8 onwards.
-#if defined(__GNUC__)
-# if defined(__GNUC_PATCHLEVEL__)
-#  define BOOST_ATOMIC_GNUC_VERSION (__GNUC__ * 10000           \
-                                     + __GNUC_MINOR__ * 100     \
-                                     + __GNUC_PATCHLEVEL__)
-# else
-#  define BOOST_LOCKFREE_GNUC_VERSION (__GNUC__ * 10000         \
-                                     + __GNUC_MINOR__ * 100)
-# endif
-#endif
-
-#if (BOOST_LOCKFREE_GNUC_VERSION >= 40800) && (__cplusplus >= 201103L)
+#if (BOOST_GCC >= 40800) && (__cplusplus >= 201103L)
 #undef BOOST_LOCKFREE_NO_HDR_ATOMIC
 #endif
-
-#undef BOOST_LOCKFREE_GNUC_VERSION
 
 #if defined(BOOST_LOCKFREE_NO_HDR_ATOMIC)
 #include <boost/atomic.hpp>
